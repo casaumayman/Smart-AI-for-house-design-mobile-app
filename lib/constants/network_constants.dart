06e85d8a821d41c8317a66798ccda2ba270b955b
casaumayman <casaumayman1@gmail.com>
2023-04-18 11:52:45 +0700

    edit base url

--- lib/constants/network_constants.dart
+++ lib/constants/network_constants.dart
@@ -1,3 +1,4 @@
 // const networkHost = "http://192.168.50.221:5000/";
-const networkHost = "https://ai-house-design.tgl-cloud.com";
+const networkHost =
+    "https://ai-house-design.tgl-cloud.com/stable-diffusion/v1/";
 const resultImageUrl = "public/result";
